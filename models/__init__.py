a39660bea2ae632a3038f7f22f5ac5637ca18e5c
root <emedopeace99@gmail.com>
2023-12-20 15:47:42 -0800

    DBStorage place

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""create unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+
+"""This module initializes the models package
+"""
+
 from os import getenv
+from models.engine import db_storage
+from models.engine import file_storage
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
+db_storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if db_storage_type == 'db':
+    storage = db_storage.DBStorage()
+    storage.reload()
 else:
-    storage = FileStorage()
-storage.reload()
+    storage = file_storage.FileStorage()
+    storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""This is city class"""
-from sqlalchemy.ext.declarative import declarative_base
+
+""" City Module for HBNB project
+"""
+
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from os import getenv
 from sqlalchemy.orm import relationship
-from models.place import Place
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """the City class"""
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    """ The city class, contains state ID and name
+    """
+    __tablename__ = 'cities'
+
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        # Import Place model conditionally
+        from models.place import Place
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
